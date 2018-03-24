2e8fd7c5913266d1744f9569a8f7d9e775a181b2
wangshengchun <1198190260@qq.com>
2018-03-24 15:57:31 +0800

    再次增加一个Java文件

---
+++ src/main/java/com/wschun/demo/SecondFile.java
@@ -0,0 +1,10 @@
+package com.wschun.demo;
+
+/**
+ * Created by wschun on 2018/3/24.
+ */
+public class SecondFile {
+    public static void main(String[] args) {
+        System.out.println("这是第二个Java文件哦");
+    }
+}
