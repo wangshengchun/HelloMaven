41421cbb2d124efc174b5d4a787c3709c1ac41cf
wangshengchun <1198190260@qq.com>
2018-03-24 16:29:37 +0800

    提交的第三个文件

---
+++ src/main/java/com/wschun/demo/ThirdFile.java
@@ -0,0 +1,10 @@
+package com.wschun.demo;
+
+/**
+ * Created by wschun on 2018/3/24.
+ */
+public class ThirdFile {
+    public static void main(String[] args) {
+        System.out.println("thrid java");
+    }
+}
